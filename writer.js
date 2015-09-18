209155a42b140ccccbab8f119e45f604c7615900
Ivan Malopinsky <ivan@imsky.co>
2015-09-18 01:35:37 -0400

    simplify writer, add base64 encoding

--- writer.js
+++ writer.js
@@ -1,4 +1,14 @@
+var base64 = require('base-64');
+var utf8 = require('utf8');
+
+var package = require('./package');
+
+var packageVersion = package.version;
+var formatVersion = package.packdownFormatVersion;
+
 function heading (document) {
+  if (!document.title) throw Error('Document has no title');
+
   var ret = [];
 
   ret.push('# ' + document.title);
@@ -19,21 +29,43 @@ function block (file) {
     ret.push(file.description);
   }
 
-  ret.push('```' + (file.language || ''));
-  ret.push(file.content);
+  var type = [file.language || 'txt'];
+
+  if (file.safe) {
+    type.push('base64');
+  }
+
+  ret.push('```' + type.join('-'));
+
+  if (file.safe) {
+    ret.push(base64.encode(utf8.encode(file.content)));
+  } else {
+    ret.push(file.content);
+  }
+
   ret.push('```');
 
   return ret.join('\n');
 }
 
-exports.write = function (document, files) {
-  if (!Array.isArray(files)) throw Error('File list is not an array');
+module.exports = function (document) {
+  if (!Array.isArray(document.files)) throw Error('File list is not an array');
+
+  var meta = [
+    'packdown',
+    formatVersion,
+    packageVersion
+  ];
 
   var ret = [];
 
+  ret.push('<!-- ' + meta.join('-') + ' -->');
+
   ret.push(heading(document));
 
-  for(var i = 0, l = files.length; i < l; i++) {
-
+  for(var i = 0, l = document.files.length; i < l; i++) {
+    ret.push(block(document.files[i]));
   }
+
+  return ret.join('\n\n');
 };
