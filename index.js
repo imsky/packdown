37fcbe7f7258d4afad945ebece40c41d116156ea
Ivan Malopinsky <ivan@imsky.co>
2015-10-04 01:45:48 -0400

    add reader to api

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var writer = require('./lib/writer');
+var reader = require('./lib/reader');
 
-exports.write = writer;
+exports.write = function (document) {
+  var packageVersion = '0.0.0';
+  var formatVersion = 1;
+
+  var options = {
+    'packageVersion': packageVersion,
+    'formatVersion': formatVersion
+  };
+
+  return writer(document, options);
+};
+
+exports.read = function (document) {
+  return reader(document);
+};
