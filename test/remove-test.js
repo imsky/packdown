9a9f287cb6ec46dae4af0579b7f0135935bda593
Ivan Malopinsky <ivan@imsky.co>
2016-03-09 00:06:21 -0500

    add remove command tests

---
+++ test/remove-test.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+var chai = require('chai');
+var asPromised = require('chai-as-promised');
+chai.should();
+chai.use(asPromised);
+
+var removeFunction = require('../lib/remove')
+var removeCommand = require('../lib/commands/remove');
+var reader = require('../lib/reader');
+
+describe('Remove function', function () {
+ 
+});
+
+describe('Remove command', function () {
+  
+});
