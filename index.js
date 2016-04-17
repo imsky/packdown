034dfddced7d714c4449ca1db2d4f00db4b74246
Ivan Malopinsky <ivan@imsky.co>
2016-04-16 23:43:52 -0400

    factor add function into main file

--- index.js
+++ index.js
@@ -4,14 +4,32 @@ var write = require('./lib/writer');
 var read = require('./lib/reader');
 var add = require('./lib/add');
 var remove = require('./lib/remove');
-var template=  require('./lib/template');
+var template =  require('./lib/template');
 var filesToDoc = require('./lib/files-to-doc');
 
 exports.write = write;
 
 exports.read = read;
 
-exports.add = add;
+/**
+ * Add a file object to a document object
+ * @param {Object} document
+ * @param {Object} file
+ * @return A file already existing at the added path or null
+ */
+exports.add = function (document, file) {
+  var oldFile = document.files[file.name] ? document.files[file.name] : null;
+
+  document.files[file.name] = file;
+
+  if (!oldFile) {
+    document.content.push({
+      'file': file.name
+    });
+  }
+
+  return oldFile;
+};
 
 exports.remove = remove;
 
