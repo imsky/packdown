93cecaaf9dea6516380a4d904972f332c89a3ba6
Ivan Malopinsky <ivan@imsky.co>
2016-08-25 19:56:53 -0400

    move packdown example to js file

---
+++ web/packdown-site.js
@@ -0,0 +1,16 @@
+var packdownExample = '# A JavaScript example\n\
+\n\
+## /index.html\n\
+\n\
+```\n\
+<script src="hello.js"></script>\n\
+<button onclick="helloWorld()">Hello world!</button>\n\
+```\n\
+\n\
+# /hello.js\n\
+\n\
+```\n\
+function helloWorld() {\n\
+  alert(\'Hello world!\');\n\
+}\n\
+```';
